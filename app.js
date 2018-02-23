52e535251ba688e2613dec91ebe9094156af5241
Ilgu Lee <iglee2000ca@gmail.com>
2018-02-22 19:16:07 -0500

    Added index('/') and about('/about') route

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app=express();
 
+//Index Route get, post, put, delete
+app.get('/',(req,res)=>{ // '/' is equal to 'localhost:port/'
+    res.send('INDEX');
+});
+
+app.get('/about',(req,res)=>{
+    res.send('ABOUT');
+});
+
 const PORT=5000;
 
 app.listen(PORT,()=>{
