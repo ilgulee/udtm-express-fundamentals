568e4623126a8288ef99f924884ca23b253529d2
Ilgu Lee <iglee2000ca@gmail.com>
2018-02-26 04:35:01 -0500

    Fetching ideas from mongodb

--- app.js
+++ app.js
@@ -34,6 +34,15 @@ app.get('/about',(req,res)=>{
     res.render('about');
 });
 
+//Idea Index Page
+app.get('/ideas', (req, res) => {
+    Idea.find({}).sort({date:'desc'}).then(ideas=>{
+        res.render('ideas/index',{
+            ideas:ideas
+        });
+    })
+});
+
 //Add Idea Form
 app.get('/ideas/add',(req,res)=>{
     res.render('ideas/add');
