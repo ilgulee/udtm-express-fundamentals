82f4dea25ed6e6239224e0c14c8e0e1996b84721
Ilgu Lee <iglee2000ca@gmail.com>
2018-03-06 00:41:07 -0500

    Added user registeration and hash using bcryptjs. Avoid the same email address.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+//const passport = require('passport');
 const router = express.Router();
+
+// Load User Model
+require('../models/User');
+const User = mongoose.model('users');
 
 //User Login Route
 router.get('/login', (req, res) => {
@@ -29,15 +35,46 @@ router.post('/register', (req, res) => {
     }
 
     if (errors.length > 0) {
-        res.render('users/register',{
-            errors:errors,
-            name:req.body.name,
-            email:req.body.email,
-            password:req.body.password,
-            password2:req.body.password2
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
         });
-    }else{
-        res.send('passed');
+    } else {
+        User.findOne({
+                email: req.body.email
+            })
+            .then(user => {
+                if (user) {
+                    req.flash('error_msg', 'Email already registered');
+                    res.redirect('/users/register');
+                } else {
+                    let newUser = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    });
+                    //console.log(newUser)
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err;
+                            newUser.password = hash;
+                            newUser.save()
+                                .then(user => {
+                                    req.flash('success_msg', 'You are now registered and can login');
+                                    res.redirect('/users/login');
+                                }).catch(err => {
+                                    console.log(err);
+                                    return;
+                                })
+                        });
+                    })
+                }
+            });
+
+        //res.send('passed'); This causes the problem --Cannot set headers after they are sent to the client
     }
 
 });
